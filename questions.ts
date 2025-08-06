6ed6e26b96efd8ddd174edfeff84f561eb502be9
Benyam Assegdw <benyambackbox@gmail.com>
2025-08-06 17:58:13 +0300

    Add files via upload

---
+++ questions.ts
@@ -0,0 +1,71 @@
+export const llc = [
+  {
+    key: 'llcName',
+    label: 'LLC Name',
+    value: '',
+    required: true,
+    order: 1,
+  },
+  {
+    key: 'businessAddress',
+    label: 'Business Address',
+    value: '',
+    required: true,
+    order: 2,
+  },
+  {
+    key: 'registeredAgentName',
+    label: 'Registered Agent Name',
+    value: '',
+    required: false,
+    order: 3,
+  },
+  {
+    key: 'registeredAgentAddress',
+    label: 'Registered Agent Address',
+    value: '',
+    required: false,
+    order: 4,
+  },
+  {
+    key: 'firstName',
+    label: 'First name',
+    value: 'Alex',
+    required: true,
+    order: 5,
+  },
+  {
+    key: 'articlesOfOrganization',
+    label: 'Articles of Organization',
+    value: '',
+    required: true,
+    order: 6,
+  },
+  {
+    key: 'EIN',
+    label: 'Employer Identification Number',
+    value: '',
+    required: true,
+    order: 7,
+  },
+  {
+    key: 'initialInvestment',
+    label: 'Initial Investment',
+    value: '',
+    required: true,
+    order: 8,
+  }
+];
+
+const BOC = [
+  {
+    key: 'llcName',
+    label: 'LLC Name',
+    value: '',
+    required: true,
+    order: 1,
+  },
+  {
+
+  }
+]
